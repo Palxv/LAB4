e0105082cd531537f77551c1782d01c91a4b3e09
Palxv <20pashapasha18@gmail.com>
2025-12-23 22:51:46 +0200

    Забыл Фибо

---
+++ FIBO/FIBO/FIBO.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+    int N;
+    cout << "N = ";
+    cin >> N;
+    int f0 = 0, f1 = 1, f;
+    if (N == 0) {
+        f = 0;
+    }
+    else if (N == 1) {
+        f = 1;
+    }
+    else {
+        for (int i = 2; i <= N; i++) {
+            f = f0 + f1;
+            f0 = f1;
+            f1 = f;
+        }
+    }
+    cout << "F(" << N << ") = " << f << endl;
+    return 0;
+}
